6a762ff04fcc374f4998d09e6dda29387a222f6b
Noe JOBARD <jobardn@iut-acy.local>
2021-05-26 08:29:47 +0200

    changement de version

---
+++ Projet V2019.3/Assets/Scipts/Script menu/scriptMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scriptMenu : MonoBehaviour
+{
+    public string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetScene(string sceneName)
+    {
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+
+    public void Quitter()
+    {
+        Application.Quit();
+    }
+}
